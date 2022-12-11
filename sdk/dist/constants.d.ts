b0e1ff2bbc564994c5d256493feeb4941b69ecc3
professormint <professormintology@gmail.com>
2022-12-11 22:07:15 +1300

    Remove dist

--- sdk/dist/constants.d.ts
+++
@@ -1,7 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-export declare const IDENTIFIERS_PROGRAM_ID: PublicKey;
-export declare const MULTIGRAPH_PROGRAM_ID: PublicKey;
-export declare const PROFILES_PROGRAM_ID: PublicKey;
-export declare const LEAF_PROGRAM_ID: PublicKey;
-export declare const ALIGN_PROGRAM_ID: PublicKey;
-//# sourceMappingURL=constants.d.ts.map
