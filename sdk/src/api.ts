1285bf8384b4d281c56e6b017a976af22912b288
professormint <professormintology@gmail.com>
2022-12-12 00:20:43 +1300

    Governance api

--- sdk/src/api.ts
+++ sdk/src/api.ts
@@ -1,27 +1,235 @@
-import { PublicKey } from "@solana/web3.js";
-import { Derivation} from "./pda";
-import { AlignPrograms } from "./types";
+import { sha256 } from "js-sha256";
+import { AccountInfo, PublicKey } from "@solana/web3.js";
+import { ALIGN_PROGRAM_ID } from "./constants";
+import { filterFactory } from "./filters";
+import { Derivation } from "./pda";
+import {
+    Account,
+    AlignPrograms,
+    ContributionRecord,
+    Identity,
+    Organisation,
+    OwnerRecord,
+    Profile,
+    User,
+} from "./types";
 
 export namespace Api {
-    
-    export const fetchUserProfileByIdentifier = async (identifierAddress : PublicKey, programs : AlignPrograms ) => {
+    export const fetchUserProfileByIdentifier = async (
+        identifierAddress: PublicKey,
+        programs: AlignPrograms
+    ): Promise<Account<User>> => {
+        const userProfileAddress =
+            Derivation.deriveUserProfileAddress(identifierAddress);
+        const profile: User = await programs.profilesProgram.account.user.fetch(
+            userProfileAddress
+        );
+        return {
+            address: userProfileAddress,
+            account: profile,
+        };
+    };
 
-        const userProfileAddress = Derivation.deriveUserProfileAddress(identifierAddress)
-        const profile = await programs.profilesProgram.account.user.fetch(userProfileAddress)
-        return profile
-    }
+    export const fetchUserProfileByOwnerPubkey = async (
+        ownerAddress: PublicKey,
+        programs: AlignPrograms
+    ): Promise<Account<User>> => {
+        const ownerRecord: Account<OwnerRecord> = await fetchOwnerRecord(
+            ownerAddress,
+            programs
+        );
+        const userProfile = await fetchUserProfileByIdentifier(
+            ownerRecord.account.identifier,
+            programs
+        );
+        return userProfile;
+    };
 
-    export const fetchUserProfileByOwnerPubkey= async (ownerAddress : PublicKey, programs : AlignPrograms ) => {
+    export const fetchOwnerRecord = async (
+        ownerAddress: PublicKey,
+        programs: AlignPrograms
+    ): Promise<Account<OwnerRecord>> => {
+        const ownerRecordAddress =
+            Derivation.deriveOwnerRecordAddress(ownerAddress);
+        const ownerRecord: OwnerRecord =
+            await programs.identifiersProgram.account.ownerRecord.fetch(
+                ownerRecordAddress
+            );
+        return {
+            address: ownerRecordAddress,
+            account: ownerRecord,
+        };
+    };
 
-        const ownerRecord = await fetchOwnerRecord(ownerAddress, programs);
-        const userProfile = await fetchUserProfileByIdentifier(ownerRecord.identifier, programs)
-        return userProfile
+    export const fetchOrganisationAddressesByCollections = async (
+        collectionMints: PublicKey[],
+        programs: AlignPrograms
+    ) => {
+        const orgPromises = collectionMints.map((mint) =>
+            programs.provider.connection.getProgramAccounts(ALIGN_PROGRAM_ID, {
+                dataSlice: { offset: 0, length: 0 },
+                filters: [
+                    filterFactory(
+                        0,
+                        Buffer.from(
+                            sha256.digest("account:Organisation")
+                        ).subarray(0, 8)
+                    ),
+                    filterFactory(40, mint.toBuffer()),
+                ],
+            })
+        );
 
-    }
+        const orgAccounts = await Promise.all(orgPromises);
 
-    export const fetchOwnerRecord = async (ownerAddress : PublicKey, programs : AlignPrograms) => {
-        const ownerRecordAddress = Derivation.deriveOwnerRecordAddress(ownerAddress);
-        const ownerRecord = programs.identifiersProgram.account.ownerRecord.fetch(ownerRecordAddress)
-        return ownerRecord
-    }
+        return orgAccounts
+            .flat()
+            .map(
+                (acc: { pubkey: PublicKey; account: AccountInfo<Buffer> }) =>
+                    acc.pubkey
+            );
+    };
+
+    export const fetchOrgranisation = async (
+        organisationAddress: PublicKey,
+        programs: AlignPrograms
+    ): Promise<Account<Organisation>> => {
+        const org: Organisation =
+            await programs.alignGovernanceProgram.account.organisation.fetch(
+                organisationAddress
+            );
+        return {
+            address: organisationAddress,
+            account: org,
+        };
+    };
+
+    export const fetchOrgranisations = async (
+        organisationAddresses: PublicKey[],
+        programs: AlignPrograms
+    ): Promise<Account<Organisation>[]> => {
+        const org: Organisation[] =
+            (await programs.alignGovernanceProgram.account.organisation.fetchMultiple(
+                organisationAddresses
+            )) as Organisation[];
+        return org
+            .map((org, i) => ({
+                address: organisationAddresses[i],
+                account: org,
+            }))
+            .filter((x) => x.account !== null);
+    };
+
+    export const fetchOrganisationProposals = async (
+        organisation: PublicKey,
+        programs: AlignPrograms
+    ): Promise<Account<Organisation>[]> => {
+        const proposalAccounts =
+            await programs.provider.connection.getProgramAccounts(
+                ALIGN_PROGRAM_ID,
+                {
+                    filters: [
+                        filterFactory(
+                            0,
+                            Buffer.from(
+                                sha256.digest("account:Proposal")
+                            ).subarray(0, 8)
+                        ),
+                        filterFactory(8 + 1, organisation.toBuffer()),
+                    ],
+                }
+            );
+
+        const parsedAccountData = proposalAccounts.map((acc) => {
+            const parsedAccountData: Organisation =
+                programs.alignGovernanceProgram.coder.accounts.decode(
+                    "proposal",
+                    acc.account.data as Buffer
+                ) as Organisation;
+
+            return {
+                address: acc.pubkey,
+                account: parsedAccountData,
+            };
+        });
+
+        return parsedAccountData;
+    };
+
+    export const fetchIdentifiersVotesForProposal = async (
+        userIdentifier: PublicKey,
+        proposalAddress: PublicKey,
+        programs: AlignPrograms
+    ): Promise<Account<ContributionRecord>> => {
+        const contributionRecordAddress = Derivation.deriveContributionRecord(
+            userIdentifier,
+            proposalAddress
+        );
+
+        const contributionRecord: ContributionRecord =
+            await programs.alignGovernanceProgram.account.contributionRecord.fetch(
+                contributionRecordAddress
+            );
+
+        return {
+            address: contributionRecordAddress,
+            account: contributionRecord,
+        };
+    };
+
+    export const fetchIdentifiersVotesForProposals = async (
+        userIdentifier: PublicKey,
+        proposalAddresses: PublicKey[],
+        programs: AlignPrograms
+    ): Promise<Account<ContributionRecord>[]> => {
+        const contributionRecordAddresses: PublicKey[] = proposalAddresses.map(
+            (proposalAddress) =>
+                Derivation.deriveContributionRecord(
+                    userIdentifier,
+                    proposalAddress
+                )
+        );
+
+        const contributionRecords: ContributionRecord[] =
+            (await programs.alignGovernanceProgram.account.contributionRecord.fetchMultiple(
+                contributionRecordAddresses
+            )) as ContributionRecord[];
+
+        return contributionRecords
+            .map((record, i) => ({
+                address: contributionRecordAddresses[i],
+                account: record,
+            }))
+            .filter((x) => x.account !== null);
+    };
+
+    export const fetchIdentityInfo = async (
+        userIdentifier: PublicKey,
+        programs: AlignPrograms
+    ): Promise<Account<Identity>> => {
+        const identityAddress =
+            Derivation.deriveIdentityAddress(userIdentifier);
+        const identity =
+            await programs.identifiersProgram.account.identity.fetch(
+                identityAddress
+            );
+
+        return {
+            address: identityAddress,
+            account: identity,
+        };
+    };
+
+    export const fetchNativeTreasuryBalance = async (
+        organisation: PublicKey,
+        programs: AlignPrograms
+    ): Promise<number> => {
+        const nativeTreasuryAddress =
+            Derivation.deriveNativeTreasuryAddress(organisation);
+        const balance =
+            await programs.alignGovernanceProgram.provider.connection.getBalance(
+                nativeTreasuryAddress
+            );
+        return balance;
+    };
 }
--- sdk/src/types.ts
+++ sdk/src/types.ts
@@ -1,4 +1,5 @@
-import { Program, AnchorProvider } from "@project-serum/anchor";
+import { Program, AnchorProvider, BN } from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { AlignGovernance } from "./idls/align_governance";
 import { Identifiers } from "./idls/identifiers";
 import { Leaf } from "./idls/leaf";
@@ -12,14 +13,80 @@ export enum EdgeRelation {
 
 export enum ConnectionType {
     SocialRelation,
-    Interaction
+    Interaction,
 }
 
 export interface AlignPrograms {
-    alignGovernanceProgram : Program<AlignGovernance>
-    identifiersProgram : Program<Identifiers>
-    multigraphProgram : Program<Multigraph>
-    profilesProgram : Program<Profiles>
-    leafProgram : Program<Leaf>
-    provider : AnchorProvider
+    alignGovernanceProgram: Program<AlignGovernance>;
+    identifiersProgram: Program<Identifiers>;
+    multigraphProgram: Program<Multigraph>;
+    profilesProgram: Program<Profiles>;
+    leafProgram: Program<Leaf>;
+    provider: AnchorProvider;
+}
+
+export interface Organisation {
+    identifier: PublicKey;
+    collectionMint: PublicKey;
+    subOrganisationCount: number;
+    bump: number;
+}
+
+export interface Profile {
+    displayName: string;
+    pfp: PublicKey;
+}
+
+export interface User {
+    identifier: PublicKey;
+    profile: Profile;
+    username: String;
+    bump: number;
+}
+
+export interface OwnerRecord {
+    identifier: PublicKey;
+    isDelegate: boolean;
+    // Have we signed using the account getting delegated to avoid spoofing
+    isVerified: boolean;
+    account: PublicKey;
+    // Program that owns the account, system for wallet address or programId of pda owner
+    keyAccountOwner: PublicKey;
+    reserved: number[];
+    bump: number;
+}
+
+export interface ContributionRecord {
+    idenitfier: PublicKey;
+    organisation: PublicKey; // Sub org or Organisation
+    proposal: PublicKey; // Account governance that determines council
+    isClaimed: boolean;
+    upVoteCount: BN;
+    downVoteCount: BN;
+    bump: number;
+}
+
+export interface Identity {
+    identifier: PublicKey;
+    owner: PublicKey;
+    isInRecovery: boolean;
+    recoveryKey: PublicKey | null;
+    recoveryCount: number;
+    reserved: number[];
+    // Future proofing would be nice to have DID cabpabilities
+    // but need to consult brian for his expertise
+    // http://g.identity.com/sol-did/
+    did: string | null;
+    bump: number;
+}
+
+export interface Account<T> {
+    address: PublicKey;
+    account: T;
+}
+
+export interface ProposalData {
+    name: string;
+    description: string;
+    image: string;
 }
