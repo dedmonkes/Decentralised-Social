ffa2d66d586955147887f7c197f6ae1fcbab68cb
professormint <97988704+professormint@users.noreply.github.com>
2022-12-11 22:16:36 +1300

    Fetch profiles (#9)

--- sdk/src/types.ts
+++ sdk/src/types.ts
@@ -1,3 +1,10 @@
+import { Program, AnchorProvider } from "@project-serum/anchor";
+import { AlignGovernance } from "./idls/align_governance";
+import { Identifiers } from "./idls/identifiers";
+import { Leaf } from "./idls/leaf";
+import { Multigraph } from "./idls/multigraph";
+import { Profiles } from "./idls/profiles";
+
 export enum EdgeRelation {
     Asymmetric,
     Symmetric,
@@ -6,4 +13,13 @@ export enum EdgeRelation {
 export enum ConnectionType {
     SocialRelation,
     Interaction
+}
+
+export interface AlignPrograms {
+    alignGovernanceProgram : Program<AlignGovernance>
+    identifiersProgram : Program<Identifiers>
+    multigraphProgram : Program<Multigraph>
+    profilesProgram : Program<Profiles>
+    leafProgram : Program<Leaf>
+    provider : AnchorProvider
 }
